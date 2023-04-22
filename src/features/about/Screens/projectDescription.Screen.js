7226feec4cae6acb8ccc09e63b3665a5374a6652
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-04-22 23:53:15 +0200

    handle keyExtractor

--- src/features/about/Screens/projectDescription.Screen.js
+++ src/features/about/Screens/projectDescription.Screen.js
@@ -36,7 +36,7 @@ export const ProjectDescriptionScreen = () => {
             </Row>
           );
         }}
-        keyExtractor={(item) => item.index}
+        keyExtractor={(item) => item.body}
       />
     </SafeArea>
   );
